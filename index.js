518d0f7c1ffc8d620afdf69bdbe1a37dad3e35d9
emapatacko <emapatacko1@gmail.com>
2026-04-25 14:35:28 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.on('ready', () => {
+  console.log(`Bot je online kao ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content === 'ping') {
+    message.reply('pong!');
+  }
+});
+
+client.login(process.env.TOKEN);
